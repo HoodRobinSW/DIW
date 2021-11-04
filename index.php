592948861a54eb91321544ad2a16faac81c78320
Alejandro <aleexortega98@gmail.com>
2021-11-04 22:04:40 +0100

    deleted bootstrap and started styles on my own

--- index.php
+++ index.php
@@ -12,66 +12,57 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link href="bootstrap-5.1.2-dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="bootstrap-5.1.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link href="bootstrap-5.1.2-dist/css/styles.css" rel="stylesheet"/>
+    <link rel="icon" href="page-icon.png" type="image/gif">
     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alejandro Ortega</title>
   </head>
   <body>
-    <header class="p-3 mb-3 border-bottom">
-      <nav id="navbar_bg" class="navbar navbar-expand-md navbar-dark fixed-top ">
-        <div class="container-fluid">
-          <div id="navbar_" class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a id="list_item_" href="" class="nav-link px-2 link-dark">Home</a></li>
-              <!--<li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
-              <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-              <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>-->
-            </ul>
-            <!--THIS TOGGLES WHEN THE USER LOGINS; DISPLAY NONE;-->
-            <div id="login_signup_" style='<?php echo $_SESSION['login_signup_display_style']; ?>'>
-              <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a id="list_item_" class="nav-link px-2 link-dark" href="login/">Login</a></li>
-                <li><a id="list_item_" class="nav-link px-2 link-dark" href="form/">Sign up</a></li>
-              </ul>
-            </div>
-            <!--THIS TOGGLES WHEN THE USER LOGINS-->
-            <div id="login_dropdown_" class="dropdown text-end" style="<?php echo $_SESSION['display_user_style']; ?>">
-              <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="<?php if (isset($_SESSION['profile_image'])) {
-                  echo 'profile/uploads/' . $_SESSION['profile_image'];} else {echo 'images/profile-silhouette.png';}?>" class="rounded-circle" width="32" height="32">
-              </a>
-              <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="profile">Profile</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="login/logout.php">Sign out</a></li>
-              </ul>
-            </div>
-            <!--UNTIL HERE-->
+    <header>
+      <nav>
+        <div id="navbar_" class="center_item">
+          <div class="homeButton-container center_item_vertically">
+            <a id="list_item_" href="" class="">Home</a>
           </div>
+
+          <!--THIS TOGGLES WHEN THE USER LOGINS; DISPLAY NONE;-->
+          <div id="login_signup_" style='<?php echo $_SESSION['login_signup_display_style']; ?>'>
+            <ul class="main-ul-container">
+              <li><a id="list_item_" class="" href="login/">Login</a></li>
+              <li><a id="list_item_" class="" href="form/">Sign up</a></li>
+            </ul>
+          </div>
+          <!--THIS TOGGLES WHEN THE USER LOGINS-->
+          <div id="login_dropdown_" class="dropdown text-end" style="<?php echo $_SESSION['display_user_style']; ?>">
+            <a href="#" class="" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="<?php if (isset($_SESSION['profile_image'])) {
+                echo 'profile/uploads/' . $_SESSION['profile_image'];} else {echo 'images/profile-silhouette.png';}?>" width="32" height="32">
+            </a>
+            <ul class="userDropDown" aria-labelledby="dropdownUser1">
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><a class="dropdown-item" href="profile">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="login/logout.php">Sign out</a></li>
+            </ul>
+          </div>
+          <!--UNTIL HERE-->
         </div>
+
       </nav>
   </header>
     <main>
-      <div class="container">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="profile.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" width="700" height="500">
-      </div>
-      <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3">Welcome to my page!</h1>
-        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap,
-          the world’s most popular front-end open source toolkit, featuring Sass variables and mixins,
-          responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+      <div class="main_container">
+        <div id="frontPage_image">
+          <img src="profile.png" alt="Alejandro Ortega Guerra" loading="lazy" width="300" height="300">
+        </div>
+        <div id="frontPage_text">
+          <h1>Welcome to my page!</h1>
+          <p class="center_item_vertically">Quickly design and customize responsive mobile-first sites with Bootstrap,
+            the world’s most popular front-end open source toolkit, featuring Sass variables and mixins,
+            responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
         </div>
       </div>
-    </div>
-  </div>
     </main>
   </body>
 </html>
--- login/index.php
+++ login/index.php
@@ -5,10 +5,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link href="../bootstrap-5.1.2-dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="../bootstrap-5.1.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link href="../bootstrap-5.1.2-dist/css/styles.css" rel="stylesheet"/>
+    <link rel="icon" href="../page-icon.png" type="image/gif">
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
   </head>
   <body>
     <?php
@@ -47,13 +48,13 @@
               $_SESSION['display_user_style'] = 'display: block;';
               header('Location: ../');
             } else {
-              $errorSignIn = "Error, verifique su email o contraseña";
+              $errorSignIn = "Error, check your email/password";
             }
           } else {
             $verificationError = "visibility: visible";
           }
         } else {
-          $errorSignIn = "Error, verifique su email o contraseña";
+          $errorSignIn = "Error, check your email or password";
         }
 
       }
@@ -61,74 +62,97 @@
       if (!empty($errorSignIn))
         $errorSignIp_style = 'display: block; opacity: 1;';
      ?>
-     <header class="p-3 mb-3 border-bottom">
-       <nav id="navbar_bg" class="navbar navbar-expand-md navbar-dark fixed-top ">
-         <div class="container-fluid">
-           <div id="navbar_" class="collapse navbar-collapse" id="navbarCollapse">
-             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-               <li><a id="list_item_" href="../" class="nav-link px-2 link-dark">Home</a></li>
-               <!--<li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
-               <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-               <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>-->
-             </ul>
-             <!--THIS TOGGLES WHEN THE USER LOGINS; DISPLAY NONE;-->
-             <div id="login_signup_" style="<?php echo $_SESSION['login_signup_display_style'];?>">
-               <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                 <li><a id="list_item_" class="nav-link px-2 link-dark" href="">Login</a></li>
-                 <li><a id="list_item_" class="nav-link px-2 link-dark" href="../form/">Sign up</a></li>
-               </ul>
-             </div>
-             <!--THIS TOGGLES WHEN THE USER LOGINS-->
-             <div id="login_dropdown_" class="dropdown text-end" style="<?php echo $_SESSION['display_user_style'];?>">
-               <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                 <img src="<?php if (isset($_SESSION['profile_image'])) {
-                   echo 'uploads/' . $_SESSION['profile_image'];} else {echo '../images/profile-silhouette.png';}?>" alt="mdo" class="rounded-circle" width="32" height="32">
-               </a>
-               <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                 <li><a class="dropdown-item" href="#">Settings</a></li>
-                 <li><a class="dropdown-item" href="#">Profile</a></li>
-                 <li><hr class="dropdown-divider"></li>
-                 <li><a class="dropdown-item" href="logout.php">Sign out</a></li>
-               </ul>
-             </div>
-             <!--UNTIL HERE-->
+     <header>
+       <nav>
+         <div id="navbar_" class="center_item">
+           <div class="homeButton-container center_item_vertically">
+             <a id="list_item_" href="../" class="">Home</a>
            </div>
+
+           <!--THIS TOGGLES WHEN THE USER LOGINS; DISPLAY NONE;-->
+           <div id="login_signup_" style='<?php echo $_SESSION['login_signup_display_style']; ?>'>
+             <ul class="main-ul-container">
+               <li><a id="list_item_" class="" href="">Login</a></li>
+               <li><a id="list_item_" class="" href="../form/">Sign up</a></li>
+             </ul>
+           </div>
+           <!--THIS TOGGLES WHEN THE USER LOGINS-->
+           <div id="login_dropdown_" class="dropdown text-end" style="<?php echo $_SESSION['display_user_style']; ?>">
+             <a href="#" class="" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+               <img src="<?php if (isset($_SESSION['profile_image'])) {
+                 echo 'profile/uploads/' . $_SESSION['profile_image'];} else {echo 'images/profile-silhouette.png';}?>" width="32" height="32">
+             </a>
+             <ul class="userDropDown" aria-labelledby="dropdownUser1">
+               <li><a class="dropdown-item" href="#">Settings</a></li>
+               <li><a class="dropdown-item" href="profile">Profile</a></li>
+               <li><hr class="dropdown-divider"></li>
+               <li><a class="dropdown-item" href="login/logout.php">Sign out</a></li>
+             </ul>
+           </div>
+           <!--UNTIL HERE-->
          </div>
        </nav>
-   </header>
-    <main class="form-signin">
+     </header>
+    <main>
       <h2 class="signupFormHeader">Log in</h2>
+      <!--Error Window-->
       <div class="signupError" style="<?php echo $errorSignIp_style ?>">
-        <?php echo $errorSignIn  ?>
-      </div>
-      <div class="text-center signupFormHeader">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-          <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" name="email" value="<?php echo $email;?>" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" name="pass" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
+        <div class="center_item">
+          <?php echo $errorSignIn  ?>
+        </div>
 
-          <div class="checkbox mb-3" style="margin: 1rem 0;">
-            <label>
-              <input type="checkbox" value="remember-me"> Remember me
-            </label>
+      </div>
+      <!---->
+      <div class="login_form">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+          <div class="form_entry someSpace">
+            <div class="center_form_entry">
+              <div class="labeldiv">
+                <label>Email address</label>
+              </div>
+              <div class="inputdiv">
+                <input type="email" name="email" value="<?php echo $email;?>" placeholder="mail@example.com">
+              </div>
+            </div>
           </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <div class="form_entry someSpace">
+            <div class="center_form_entry">
+              <div class="labeldiv">
+                <label>Password</label>
+              </div>
+              <div class="inputdiv">
+                <input type="password" name="pass" placeholder="Password">
+              </div>
+            </div>
+          </div>
+          <div class="form_entry someSpace">
+            <div class="center_form_entry">
+              <div class="rememberdiv">
+                Remember me
+              </div>
+              <div style="text-align:center;margin-top:0.5rem;">
+                <input type="checkbox" value="remember-me">
+              </div>
+            </div>
+          </div>
+          <div class="form_entry someSpace">
+            <div class="center_form_entry">
+              <div class="login_button_div">
+                <button type="submit">Sign in</button>
+              </div>
+            </div>
+          </div>
         </form>
       </div>
       <!--Modals-->
-      <div class="modal modal-alert position-fixed d-block bg-secondary py-5" tabindex="-1" role="dialog" id="modalChoice" style="<?php echo $verificationError;?>">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content rounded-4 shadow">
-            <div class="modal-body p-4 text-center">
-              <h5 class="mb-0">Email verification required</h5>
-              <p class="mb-0">We've already sent you a verification email, please check your inbox</p>
+      <div class="modal center_item" role="dialog" id="modalChoice" style="<?php echo $verificationError;?>">
+        <div class="modal-dialog center_item" role="document">
+          <div>
+            <div>
+              <h3 class="modal-title">Email verification required</h3>
+              <p class="modal-text">We've already sent you a verification email, please check your inbox</p>
             </div>
-            <div class="modal-footer flex-nowrap p-0">
+            <div class="modal-footer">
               <button type="button" class="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-right"><strong>Resend</strong></button>
               <button type="button" class="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0" data-bs-dismiss="modal" onclick="document.getElementById('modalChoice').style.visibility = 'hidden'">Close</button>
             </div>
